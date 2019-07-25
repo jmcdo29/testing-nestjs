f373658a324900f49ee57bdb51cb343bb72c0972
Jay McDoniel <jmcdo29@gmail.com>
2019-07-25 16:10:42 -0700

    feat(simple-sample): update simple -sample tests
    Update simple-samples tests to cover all coner cases of a very
    basic server. Includes Unit Tests for controller and service
    and e2e tests to cover entire project.

--- simple-sample/src/app.controller.ts
+++ simple-sample/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query('name') name?: string): string {
+    return this.appService.getHello(name);
   }
 }
--- simple-sample/test/app.e2e-spec.ts
+++ simple-sample/test/app.e2e-spec.ts
@@ -1,23 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { INestApplication } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
-  let app;
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/ (GET)', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello, World!');
+    });
+    it('/?name=Tester', () => {
+      return request(app.getHttpServer())
+        .get('/?name=Tester')
+        .expect(200)
+        .expect('Hello, Tester!');
+    });
   });
 });
