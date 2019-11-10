20f0131958c041cbad1ec75e2367a9a45488c5fe
Jay McDoniel <jmcdo29@gmail.com>
2019-11-10 15:24:15 -0800

    test(mock): failed mocking with createMock with DI

---
+++ apps/typeorm-sample/src/cat/cat.service.create-mock.spec.ts
@@ -0,0 +1,55 @@
+import { createMock } from '@golevelup/nestjs-testing';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cat } from './cat.entity';
+import { CatService } from './cat.service';
+
+describe('CatService using createMock with DI', () => {
+  let service: CatService;
+  let repo: Repository<Cat>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        CatService,
+        {
+          provide: getRepositoryToken(Cat),
+          useValue: createMock<Repository<Cat>>(),
+        },
+      ],
+    }).compile();
+
+    service = module.get<CatService>(CatService);
+    repo = module.get<Repository<Cat>>(getRepositoryToken(Cat));
+  });
+
+  it('should have the repo mocked', () => {
+    expect(typeof repo.find).toBe('function');
+  });
+});
+
+describe('CatService using createMock without DI', () => {
+  let service: CatService;
+  const repo = createMock<Repository<Cat>>();
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        CatService,
+        {
+          provide: getRepositoryToken(Cat),
+          useValue: repo,
+        },
+      ],
+    }).compile();
+
+    service = module.get<CatService>(CatService);
+  });
+
+  it('should have the repo mocked', async () => {
+    const cat = { name: 'cat1', age: 2, breed: 'tabby', id: '1' };
+    repo.find.mockResolvedValue([cat]);
+    expect(await repo.find()).toEqual([cat]);
+  });
+});
