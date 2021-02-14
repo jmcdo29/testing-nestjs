bc92d651f5b25adc75afd74b1e348cf4e67688d9
Lumm1t <lummit1337@gmail.com>
2021-02-14 19:39:24 +0100

    refactor(imports): use shorthand for app.module

--- apps/graphql-sample/test/app.e2e-spec.ts
+++ apps/graphql-sample/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { Cat } from '../src/cat/models/cat-query.dto';
 import { INestApplication } from '@nestjs/common';
 
