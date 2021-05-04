da47fd0ec4cbd2fa5551cc10f04a828709319c50
Isaac Souza <isaacsouza@guia.dev>
2021-05-04 16:11:17 -0300

    updated

--- apps/simple-sample/src/app.service.ts
+++ apps/simple-sample/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(name?: string): string {
     return `Hello, ${name || 'World'}!`;
   }
+
+  getHelloNew(name?: string): string {
+    return `Hello, ${name || 'World'}!`;
+  }
+
+  getHelloOld(name?: string): string {
+    return `Hello, ${name || 'World'}!`;
+  }
 }
