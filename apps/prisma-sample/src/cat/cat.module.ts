a66ffa78c92ffb5b1832ed56f1f84c3f510501b0
Benjamin <benny.klotz92@gmail.com>
2021-06-25 20:22:19 +0200

    chore(prisma-sample): Use PrismaService without custom injection token, just export it from the global module

--- apps/prisma-sample/src/cat/cat.module.ts
+++ apps/prisma-sample/src/cat/cat.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { CatController } from './cat.controller';
 import { CatService } from './cat.service';
 
 @Module({
-  providers: [PrismaService, CatService],
+  providers: [CatService],
   controllers: [CatController],
 })
 export class CatModule {}
--- apps/prisma-sample/src/prisma/prisma.module.ts
+++ apps/prisma-sample/src/prisma/prisma.module.ts
@@ -1,14 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-const prismaClient = {
-  provide: 'PrismaClient',
-  useClass: PrismaService,
-};
-
 @Global()
 @Module({
-  providers: [prismaClient],
-  exports: [prismaClient],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
