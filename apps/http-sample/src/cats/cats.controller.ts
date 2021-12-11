6091778d00e017feda9b2a9d046c4dd2a00b7132
Tony133 <a.tripodi133@gmail.com>
2021-12-11 22:48:14 +0100

    feat(): update cats.controller.ts

--- apps/http-sample/src/cats/cats.controller.ts
+++ apps/http-sample/src/cats/cats.controller.ts
@@ -10,33 +10,39 @@ import {
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Observable } from 'rxjs';
+import { Cat } from './entities/cat.entity';
+import { AxiosResponse } from 'axios';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
-    return await this.catsService.create(createCatDto);
+  create(@Body() createCatDto: CreateCatDto): Observable<AxiosResponse<Cat[]>> {
+    return this.catsService.create(createCatDto);
   }
 
   @Get()
-  async findAll() {
-    return await this.catsService.findAll();
+  findAll(): Observable<AxiosResponse<Cat[]>> {
+    return this.catsService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.catsService.findOne(+id);
+  findOne(@Param('id') id: string): Observable<AxiosResponse<Cat>> {
+    return this.catsService.findOne(+id);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return await this.catsService.update(+id, updateCatDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateCatDto: UpdateCatDto,
+  ): Observable<AxiosResponse<Cat>> {
+    return this.catsService.update(+id, updateCatDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.catsService.remove(+id);
+  remove(@Param('id') id: string): Observable<AxiosResponse<any>> {
+    return this.catsService.remove(+id);
   }
 }
