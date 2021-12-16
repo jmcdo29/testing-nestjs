66febbcfaf9a543a2ce6dbf84cbc3b009ed85e84
Tony <Tony133@users.noreply.github.com>
2021-12-16 23:28:13 +0100

    feat(): added http sample (#1478)
    Co-authored-by: Jay McDoniel <jmcdo29@gmail.com>
    Co-authored-by: Webbly25 <79157185+Webbly25@users.noreply.github.com>

---
+++ apps/http-sample/src/app.module.ts
@@ -0,0 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ apps/http-sample/src/cats/cats.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { Observable } from 'rxjs';
+import { Cat } from './entities/cat.entity';
+import { AxiosResponse } from 'axios';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): Observable<AxiosResponse<Cat[]>> {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  findAll(): Observable<AxiosResponse<Cat[]>> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<AxiosResponse<Cat>> {
+    return this.catsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCatDto: UpdateCatDto,
+  ): Observable<AxiosResponse<Cat>> {
+    return this.catsService.update(+id, updateCatDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Observable<AxiosResponse<any>> {
+    return this.catsService.remove(+id);
+  }
+}
