a6cef5323392d6dc6a55a8ff63baa3f9ac2cc5fb
Jay McDoniel <jmcdo29@gmail.com>
2021-12-31 13:28:04 -0800

    chore: migrate to nx monorepo
    I'm moving over from a Nest monorepo to an [nx][1] monorepo to allow for
    easier development and more robust commands. The ability to use `npm run nx affected:test`
    to take care of all of the changed tests at once is incredibly useful. Also, it helps with
    running the projects locally via compiling with webpack, and generally has a better DX in
    my honest opinion. Plus, it can allow for a useful refactor pattern later, and better exposure
    to using Nx.
    
    closes #1222
    
    [1]: https://nx.dev

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*spec.ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: ['TS151001'],
+      },
+      tsconfig: '<rootDir>/tsconfig.app.json',
+    },
+  },
+};
