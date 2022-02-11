d50ae660c82a60484021731605c5690174142022
Tony133 <a.tripodi133@gmail.com>
2022-02-11 22:42:09 +0100

    sample(graphql-sample): update version graphql

--- apps/graphql-sample/src/app.module.ts
+++ apps/graphql-sample/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CatModule } from './cat/cat.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     CatModule,
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: true,
       playground: true,
       debug: true,
