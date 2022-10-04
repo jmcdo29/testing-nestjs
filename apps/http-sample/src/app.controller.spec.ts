912f11c2ca66b2dda096815ae706e2683ee8e8ae
singharsh0 <51259121+singharsh0@users.noreply.github.com>
2022-10-04 21:21:40 +0530

    correct unit test

--- apps/http-sample/src/app.controller.spec.ts
+++ apps/http-sample/src/app.controller.spec.ts
@@ -16,7 +16,8 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      const response = appController.getHello();
+      expect(response).toBe('Hello World!');
     });
   });
 });
