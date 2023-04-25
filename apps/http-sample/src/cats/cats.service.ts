1cbe053cb90a03959a04042da94d7b8e8b603fe6
neerajkumar161 <ennkay161@gmail.com>
2023-04-25 18:48:47 +0530

    refactor: http-sample controller and service

--- apps/http-sample/src/cats/cats.service.ts
+++ apps/http-sample/src/cats/cats.service.ts
@@ -1,6 +1,5 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CreateCatDto } from './dto/create-cat.dto';
@@ -11,34 +10,31 @@ import { Cat } from './entities/cat.entity';
 export class CatsService {
   constructor(private httpService: HttpService) {}
 
-  create(createCatDto: CreateCatDto): Observable<AxiosResponse<any>> {
+  create(createCatDto: CreateCatDto): Observable<Cat> {
     return this.httpService
-      .post('http://localhost:3000/cats', createCatDto)
+      .post<Cat>('http://localhost:3000/cats', createCatDto)
       .pipe(map((response) => response.data));
   }
 
-  findAll(): Observable<AxiosResponse<Cat[]>> {
+  findAll(): Observable<Cat[]> {
     return this.httpService
-      .get('http://localhost:3000/cats')
+      .get<Cat[]>('http://localhost:3000/cats')
       .pipe(map((response) => response.data));
   }
 
-  findOne(id: number): Observable<AxiosResponse<Cat>> {
+  findOne(id: number): Observable<Cat> {
     return this.httpService
-      .get(`http://localhost:3000/cats/${id}`)
+      .get<Cat>(`http://localhost:3000/cats/${id}`)
       .pipe(map((response) => response.data));
   }
 
-  update(
-    id: number,
-    updateCatDto: UpdateCatDto,
-  ): Observable<AxiosResponse<Cat>> {
+  update(id: number, updateCatDto: UpdateCatDto): Observable<Cat> {
     return this.httpService
-      .put(`http://localhost:3000/cats/${id}`, updateCatDto)
+      .put<Cat>(`http://localhost:3000/cats/${id}`, updateCatDto)
       .pipe(map((response) => response.data));
   }
 
-  remove(id: number): Observable<AxiosResponse<void>> {
+  remove(id: number): Observable<void> {
     return this.httpService
       .delete(`http://localhost:3000/cats/${id}`)
       .pipe(map((response) => response.data));
