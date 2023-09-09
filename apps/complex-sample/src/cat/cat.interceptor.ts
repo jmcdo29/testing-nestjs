b6665523b37c3a37899988cad091ee7403523403
neerajkumar161 <ennkay161@gmail.com>
2023-09-09 12:30:43 +0530

    test(complex-sample): test case added and type assertions fixes

--- apps/complex-sample/src/cat/cat.interceptor.ts
+++ apps/complex-sample/src/cat/cat.interceptor.ts
@@ -6,10 +6,14 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Cat } from './models/cats';
 
 @Injectable()
 export class CatInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<Cat>,
+  ): Observable<{ data: Cat }> {
     return next.handle().pipe(map((data) => ({ data })));
   }
 }
