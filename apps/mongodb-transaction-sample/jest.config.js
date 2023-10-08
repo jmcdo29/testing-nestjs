a072ecba9333cfdeb880a6fd94a3a5432019cd1b
neerajkumar161 <ennkay161@gmail.com>
2023-10-09 00:04:35 +0530

    fix: comments removed in jest.config.js

--- apps/mongodb-transaction-sample/jest.config.js
+++ apps/mongodb-transaction-sample/jest.config.js
@@ -4,11 +4,4 @@ module.exports = {
   displayName: 'mongodb-transaction-sample',
   ...baseConfig,
   snapshotFormat: { escapeString: true, printBasicPrototype: true },
-  // preset: '../../jest.preset.js',
-  // testEnvironment: 'node',
-  // transform: {
-  //   '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  // },
-  // moduleFileExtensions: ['ts', 'js', 'html'],
-  // coverageDirectory: '../../coverage/apps/mongodb-transaction-sample',
 };
